f91e88915cf0b785e62112036847cc5895ebe7bb
Marco <rg.marcoandres@gmail.com>
2019-06-02 20:44:55 -0400

    Todas las funcionalidades agregadas

--- src/Modelo/Database.php
+++ src/Modelo/Database.php
@@ -11,13 +11,33 @@ class Database{
 	}
 
 	public function noticias(){
-		$arr = $this->database->select('noticia',['id', 'titulo', 'encabezado', 'cuerpo']);
-		return $arr;
+		$arr = $this->database->select('noticia',['id', 'titulo', 'encabezado', 'cuerpo'],['estado'=>0]);
+		echo json_encode($arr);
+	}
+
+
+
+
+
+
+
+
+
+
+
+	public function noticiasLeidas(){
+		$leidas = $this->database->select('noticia',['id', 'titulo', 'encabezado', 'cuerpo'],['estado'=>1]);
+		//echo json_encode(array('leidas' => $leidas));
+		echo json_encode($leidas);
 	}
 	
 	public function noticiaPorId($id){
-		$arr = $this->database->select('noticia',['id', 'titulo', 'encabezado', 'cuerpo']
-		,['id'=>$id]);
-		return $arr;
+		$arr = $this->database->get('noticia',['titulo','encabezado', 'cuerpo'],['id'=>$id]);
+		echo json_encode($arr);
+	}
+
+	public function cambiarEstado($id){
+		$set = $this->database->update('noticia',['estado'=>1],['id'=>$id]);
+		return $set;
 	}
 }
